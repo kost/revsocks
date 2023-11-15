80aeb3e19846e2bab3158c9eebd9c03ab6174c97
Vlatko Kosturjak <kost@linux.hr>
2023-11-15 05:36:55 +0100

    Rework arg parsing part

--- main.go
+++ main.go
@@ -26,24 +26,35 @@ type AppOptions struct {
 	recn int
 	rect int
 	optquiet bool
+	proxyauthstring string
+	optproxytimeout string
+	optdnslisten string
+	optdnsdelay string
+	optdnsdomain string
+	listen string
+	certificate string
+	socks string
+	connect string
+	proxy string
+	agentpassword string
 }
 
 var CurOptions AppOptions
 
 func main() {
-	listen := flag.String("listen", "", "listen port for receiver address:port")
-	certificate := flag.String("cert", "", "certificate file")
-	socks := flag.String("socks", "127.0.0.1:1080", "socks address:port")
-	connect := flag.String("connect", "", "connect address:port (or https://address:port for ws)")
-	proxy := flag.String("proxy", "", "use proxy address:port for connecting (or http://address:port for ws)")
-	optdnslisten := flag.String("dnslisten", "", "Where should DNS server listen")
-	optdnsdelay := flag.String("dnsdelay", "", "Delay/sleep time between requests (200ms by default)")
-	optdnsdomain := flag.String("dns", "", "DNS domain to use for DNS tunneling")
-	optproxytimeout := flag.String("proxytimeout", "", "proxy response timeout (ms)")
-	proxyauthstring := flag.String("proxyauth", "", "proxy auth Domain/user:Password ")
+	flag.StringVar(&CurOptions.listen,"listen", "", "listen port for receiver address:port")
+	flag.StringVar(&CurOptions.certificate,"cert", "", "certificate file")
+	flag.StringVar(&CurOptions.socks,"socks", "127.0.0.1:1080", "socks address:port")
+	flag.StringVar(&CurOptions.connect,"connect", "", "connect address:port (or https://address:port for ws)")
+	flag.StringVar(&CurOptions.proxy,"proxy", "", "use proxy address:port for connecting (or http://address:port for ws)")
+	flag.StringVar(&CurOptions.optdnslisten,"dnslisten", "", "Where should DNS server listen")
+	flag.StringVar(&CurOptions.optdnsdelay,"dnsdelay", "", "Delay/sleep time between requests (200ms by default)")
+	flag.StringVar(&CurOptions.optdnsdomain, "dns", "", "DNS domain to use for DNS tunneling")
+	flag.StringVar(&CurOptions.optproxytimeout,"proxytimeout", "", "proxy response timeout (ms)")
+	flag.StringVar(&CurOptions.proxyauthstring,"proxyauth", "", "proxy auth Domain/user:Password")
 	flag.StringVar(&CurOptions.autocert,"autocert","","use domain.tld and automatically obtain TLS certificate")
 	flag.StringVar(&CurOptions.useragent,"agent","Mozilla/5.0 (Windows NT 6.1; Trident/7.0; rv:11.0) like Gecko","User agent to use")
-	optpassword := flag.String("pass", "", "Connect password")
+	flag.StringVar(&CurOptions.agentpassword,"pass", "", "Connect password")
 	flag.BoolVar(&CurOptions.optquiet,"q",false,"Be quiet - do not display output")
 	flag.IntVar(&CurOptions.recn,"recn", 3, "reconnection limit")
 	flag.IntVar(&CurOptions.rect,"rect", 30, "reconnection delay")
@@ -80,15 +91,15 @@ func main() {
 		os.Exit(0)
 	}
 
-	if *optpassword != "" {
+	if CurOptions.agentpassword == "" {
 		agentpassword = RandString(64)
 		log.Printf("No password specified. Generated password is %s", agentpassword)
 	}
 
-	if *listen != "" {
+	if CurOptions.listen != "" {
 		log.Println("Starting to listen for clients")
-		if *optproxytimeout != "" {
-			opttimeout, _ := strconv.Atoi(*optproxytimeout)
+		if CurOptions.optproxytimeout != "" {
+			opttimeout, _ := strconv.Atoi(CurOptions.optproxytimeout)
 			proxytout = time.Millisecond * time.Duration(opttimeout)
 		} else {
 			proxytout = time.Millisecond * 1000
@@ -96,29 +107,29 @@ func main() {
 
 		//listenForSocks(*listen, *certificate)
 		if CurOptions.usewebsocket {
-			log.Fatal(listenForWebsocketAgents(CurOptions.usetls, *listen, *socks, *certificate, CurOptions.autocert))
+			log.Fatal(listenForWebsocketAgents(CurOptions.usetls, CurOptions.listen, CurOptions.socks, CurOptions.certificate, CurOptions.autocert))
 		} else {
-			log.Fatal(listenForAgents(CurOptions.usetls, *listen, *socks, *certificate, CurOptions.autocert))
+			log.Fatal(listenForAgents(CurOptions.usetls, CurOptions.listen, CurOptions.socks, CurOptions.certificate, CurOptions.autocert))
 		}
 	}
 
-	if *connect != "" {
+	if CurOptions.connect != "" {
 
-		if *optproxytimeout != "" {
-			opttimeout, _ := strconv.Atoi(*optproxytimeout)
+		if CurOptions.optproxytimeout != "" {
+			opttimeout, _ := strconv.Atoi(CurOptions.optproxytimeout)
 			proxytimeout = time.Millisecond * time.Duration(opttimeout)
 		} else {
 			proxytimeout = time.Millisecond * 1000
 		}
 
-		if *proxyauthstring != "" {
-			if strings.Contains(*proxyauthstring, "/") {
-				domain = strings.Split(*proxyauthstring, "/")[0]
-				username = strings.Split(strings.Split(*proxyauthstring, "/")[1], ":")[0]
-				password = strings.Split(strings.Split(*proxyauthstring, "/")[1], ":")[1]
+		if CurOptions.proxyauthstring != "" {
+			if strings.Contains(CurOptions.proxyauthstring, "/") {
+				domain = strings.Split(CurOptions.proxyauthstring, "/")[0]
+				username = strings.Split(strings.Split(CurOptions.proxyauthstring, "/")[1], ":")[0]
+				password = strings.Split(strings.Split(CurOptions.proxyauthstring, "/")[1], ":")[1]
 			} else {
-				username = strings.Split(*proxyauthstring, ":")[0]
-				password = strings.Split(*proxyauthstring, ":")[1]
+				username = strings.Split(CurOptions.proxyauthstring, ":")[0]
+				password = strings.Split(CurOptions.proxyauthstring, ":")[1]
 			}
 			log.Printf("Using domain %s with %s:%s", domain, username, password)
 		} else {
@@ -132,9 +143,9 @@ func main() {
 			for i := 1; i <= CurOptions.recn; i++ {
 				log.Printf("Connecting to the far end. Try %d of %d", i, CurOptions.recn)
 				if CurOptions.usewebsocket {
-					WSconnectForSocks(CurOptions.verify, *connect, *proxy)
+					WSconnectForSocks(CurOptions.verify, CurOptions.connect, CurOptions.proxy)
 				} else {
-					error1 := connectForSocks(CurOptions.usetls, CurOptions.verify, *connect, *proxy)
+					error1 := connectForSocks(CurOptions.usetls, CurOptions.verify, CurOptions.connect, CurOptions.proxy)
 					log.Print(error1)
 				}
 				log.Printf("Sleeping for %d sec...", CurOptions.rect)
@@ -146,9 +157,9 @@ func main() {
 			for {
 				log.Printf("Reconnecting to the far end... ")
 				if CurOptions.usewebsocket {
-					WSconnectForSocks(CurOptions.verify, *connect, *proxy)
+					WSconnectForSocks(CurOptions.verify, CurOptions.connect, CurOptions.proxy)
 				} else {
-					error1 := connectForSocks(true, CurOptions.verify, *connect, *proxy)
+					error1 := connectForSocks(CurOptions.usetls, CurOptions.verify, CurOptions.connect, CurOptions.proxy)
 					log.Print(error1)
 				}
 				log.Printf("Sleeping for %d sec...", CurOptions.rect)
@@ -160,9 +171,9 @@ func main() {
 		log.Fatal("Ending...")
 	}
 
-	if *optdnsdomain != "" {
-		dnskey:=*optpassword
-		if *optpassword == "" {
+	if CurOptions.optdnsdomain != "" {
+		dnskey:=CurOptions.agentpassword
+		if CurOptions.agentpassword == "" {
 			dnskey=GenerateKey()
 			log.Printf("No password specified, generated following (recheck if same on both sides): %s", dnskey)
 		}
@@ -170,10 +181,10 @@ func main() {
 			fmt.Fprintf(os.Stderr, "Specified key of incorrect size for DNS (should be 64 in hex)\n")
 			os.Exit(1)
 		}
-		if *optdnslisten != "" {
-			ServeDNS (*optdnslisten,*optdnsdomain,*socks, dnskey, *optdnsdelay)
+		if CurOptions.optdnslisten != "" {
+			ServeDNS (CurOptions.optdnslisten,CurOptions.optdnsdomain,CurOptions.socks, dnskey, CurOptions.optdnsdelay)
 		} else {
-			DnsConnectSocks(*optdnsdomain, dnskey, *optdnsdelay)
+			DnsConnectSocks(CurOptions.optdnsdomain, dnskey, CurOptions.optdnsdelay)
 		}
 		log.Fatal("Ending...")
 	}
